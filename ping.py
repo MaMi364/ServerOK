307bb2e22dfe5575513e824cdce26c115f3ebd8d
Muhammed <muhammedordu2002@gmail.com>
2023-04-18 18:55:33 +0200

    Tweede commit

--- ping.py
+++ ping.py
@@ -4,7 +4,6 @@ import json
 import time
 from datetime import datetime
 
-
 def myping(host, ip):
     status = None
     response = os.system("ping -c 1 " + host)
@@ -32,8 +31,6 @@ def writeToJson(file, new_data):
     data["servers"].append(new_data)
     with open(file, 'w') as outfile:
         json.dump(data, outfile, indent=4, separators=(',',': '))
-    
-
     
 def AddServer(host, ip, file):
     data = readJson(file)
@@ -71,7 +68,6 @@ def updateJson(file, updated_data):
     with open(file, 'w') as outfile:
         json.dump(data, outfile, indent=4, separators=(',',': '))
 
-
 def check(file):
     while True:
         data = readJson(file)
@@ -81,71 +77,75 @@ def check(file):
 
 
 
-
-
-
-
-
-if len(sys.argv) < 2:
-    print("Usage: python script_name.py [COMMAND] [ARGUMENTS]")
-    print("Commands:")
-    print("  add HOST IP FILE        Add a server to the file")
-    print("  remove HOST FILE        Remove a server from the file")
-    print("  show FILE               Show all servers in the file")
-else:
-    command = sys.argv[1]
-    if command == "add":
-        if len(sys.argv) < 5:
-            print("Usage: python script_name.py add HOST IP FILE")
-        else:
-            host = sys.argv[2]
-            ip = sys.argv[3]
-            file = sys.argv[4]
-            AddServer(host, ip, file)
-    elif command == "remove":
-        if len(sys.argv) < 4:
-            print("Usage: python script_name.py remove HOST FILE")
-        else:
-            host = sys.argv[2]
-            file = sys.argv[3]
-            RemoveServer(host, file)
-    elif command == "show":
-        if len(sys.argv) < 3:
-            print("Usage: python script_name.py show FILE")
-        else:
-            file = sys.argv[2]
-            showServers(file)
-    else:
-        print("Unknown command:", command)
-        
-#AddServer("www.udemy.com", "15.20.45.82", "servers.json")
-
-#showServers("servers.json")
-
-#RemoveServer("www.udemy.com", "servers.json")
-
-#check("servers.json")
-
 def main():
-    modus = input("Kies tussen Management modus en check modus \nDruk op 1 om management modus te starten \nDruk op 2 om check modus te starten ")
-    if modus == "1":
-        print()
-        vraag = input("Druk op 1 om alle servers te tonen\nDruk op 2 om een server toe te voegen\nDruk op 3 om een server te verwijderen ")
-        if vraag == "1":
-            showServers("servers.json")
-        elif vraag == "2":
-            host = input("Geef de host naam in ")
-            ip = input("Geef de host naam in ")
-            AddServer(host,ip,"servers.json")
-            print("U hebt een server toegevoegd ")
-        elif vraag == "3":
-            host = input("Geef de host naam in ")
-            RemoveServer(host, "servers.json")
+    if len(sys.argv) > 1:
+        if len(sys.argv) < 2:
+            print("Usage: python program.py [management|check]")
+            sys.exit(1)
+
+        mode = sys.argv[1]
+
+        if mode == "management":
+            if len(sys.argv) < 3:
+                print("Usage: python program.py management [show|add|remove]")
+                sys.exit(1)
+
+            action = sys.argv[2]
+
+            if action == "show":
+                showServers("servers.json")
+            elif action == "add":
+                if len(sys.argv) != 5:
+                    print("Usage: python program.py management add [host] [ip]")
+                    sys.exit(1)
+
+                host = sys.argv[3]
+                ip = sys.argv[4]
+                AddServer(host, ip, "servers.json")
+                print("Server added successfully.")
+            elif action == "remove":
+                if len(sys.argv) != 4:
+                    print("Usage: python program.py management remove [host]")
+                    sys.exit(1)
+
+                host = sys.argv[3]
+                RemoveServer(host, "servers.json")
+                print("Server removed successfully.")
+            else:
+                print("Invalid")
+                sys.exit(1)
+
+        elif mode == "check":
+            check("servers.json")
         else:
-            print("Kies tussen 1  tot 3  a.u.b.")
-    elif modus == "2":
-        print("Om de 2 minuten gaan we nu alle servers in onze database checken ")
-        check("servers.json")
+            print("Invalid mode.")
+            sys.exit(1)
+            
+    else:
+        modus = input("Kies tussen Management modus en check modus \nDruk op 1 om management modus te starten \nDruk op 2 om check modus te starten ")
+        if modus == "1":
+            print()
+            vraag = input("Druk op 1 om alle servers te tonen\nDruk op 2 om een server toe te voegen\nDruk op 3 om een server te verwijderen ")
+            if vraag == "1":
+                showServers("servers.json")
+            elif vraag == "2":
+                host = input("Geef de host naam in ")
+                ip = input("Geef de host naam in ")
+                AddServer(host,ip,"servers.json")
+                print("U hebt een server toegevoegd ")
+            elif vraag == "3":
+                host = input("Geef de host naam in ")
+                RemoveServer(host, "servers.json")
+            else:
+                print("Kies tussen 1  tot 3  a.u.b.")
+        elif modus == "2":
+            print("Om de 2 minuten gaan we nu alle servers in onze database checken ")
+            check("servers.json")
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
